d5bff3558a000f1476a7b276a3ff77073de0cdbe
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-04-28 17:57:39 +0700

    :tada: :penguin: starting version 2.0.0

---
+++ app/src/main/java/com/yoesuv/infomadiun/App.kt
@@ -0,0 +1,14 @@
+package com.yoesuv.infomadiun
+
+import android.app.Application
+
+/**
+ *  Created by yusuf on 4/28/18.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/yoesuv/infomadiun/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.yoesuv.infomadiun.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *  Created by yusuf on 4/28/18.
+ */
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
