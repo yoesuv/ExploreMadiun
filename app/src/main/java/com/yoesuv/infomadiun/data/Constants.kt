d2329dda5696c254e99b1880cabf3c522e3a0fc4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-05-01 10:45:44 +0700

    :sparkles: :penguin: setup swipe refresh list place

--- app/src/main/java/com/yoesuv/infomadiun/data/Constants.kt
+++ app/src/main/java/com/yoesuv/infomadiun/data/Constants.kt
@@ -9,4 +9,5 @@ object Constants {
 
     const val BASE_URL = "https://gist.githubusercontent.com/yoesuv/"
     const val TIME_OUT = 15 * 1000L
+    const val ANIMATION_TIME = 500L
 }
--- app/src/main/java/com/yoesuv/infomadiun/menu/listplace/views/FragmentListPlace.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/listplace/views/FragmentListPlace.kt
@@ -1,8 +1,11 @@
 package com.yoesuv.infomadiun.menu.listplace.views
 
+import android.animation.Animator
 import android.app.Activity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -11,6 +14,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import com.yoesuv.infomadiun.R
 import com.yoesuv.infomadiun.data.Constants
 import com.yoesuv.infomadiun.menu.listplace.adapters.ListPlaceAdapter
@@ -35,6 +39,7 @@ class FragmentListPlace: Fragment(), ListPlaceContract.ViewListPlace {
     private var listPlace: MutableList<PlaceModel> = arrayListOf()
     private lateinit var adapter:ListPlaceAdapter
     private var recyclerView:RecyclerView? = null
+    private lateinit var progressBar:ProgressBar
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_listplace, container, false)
@@ -43,10 +48,11 @@ class FragmentListPlace: Fragment(), ListPlaceContract.ViewListPlace {
         val toolbar = activity.findViewById<Toolbar>(R.id.toolbarMain)
         toolbar.textViewToolbar.text = getString(R.string.list_place)
 
+        setupRecycler(v)
+        setupSwipeRefresh(v)
+
         listPlacePresenter = ListPlacePresenter(this)
         listPlacePresenter.getListPlace()
-
-        setupRecycler(v)
 
         return v
     }
@@ -61,6 +67,7 @@ class FragmentListPlace: Fragment(), ListPlaceContract.ViewListPlace {
         layoutManager.orientation = LinearLayoutManager.VERTICAL
 
         recyclerView = view.findViewById(R.id.recyclerViewListPlace)
+        progressBar = view.findViewById(R.id.progressBar)
         recyclerView?.layoutManager = layoutManager
         recyclerView?.setHasFixedSize(true)
 
@@ -68,12 +75,41 @@ class FragmentListPlace: Fragment(), ListPlaceContract.ViewListPlace {
         recyclerView?.adapter = adapter
     }
 
-    override fun showLoading() {
+    private fun setupSwipeRefresh(view: View){
+        val swipeRefresh = view.findViewById<SwipeRefreshLayout>(R.id.swipeRefreshListPlace)
+        swipeRefresh.setColorSchemeColors(ContextCompat.getColor(view.context, R.color.colorPrimary))
+        swipeRefresh.setOnRefreshListener({
+            listPlacePresenter.getListPlace()
+            swipeRefresh.isRefreshing = false
+        })
+    }
 
+    override fun showLoading() {
+        progressBar.visibility = View.VISIBLE
+        progressBar.alpha = 1f
+        recyclerView?.alpha = 0f
     }
 
     override fun dismissLoading() {
+        recyclerView?.animate()?.alpha(1f)?.duration = Constants.ANIMATION_TIME
+        progressBar.animate().alpha(0f).setDuration(Constants.ANIMATION_TIME).setListener(object : Animator.AnimatorListener {
 
+            override fun onAnimationRepeat(p0: Animator?) {
+
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                progressBar.visibility = View.INVISIBLE
+            }
+        })
     }
 
     override fun setData(listPlaceModel: MutableList<PlaceModel>) {
