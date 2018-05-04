9a62bda54e8dd111bc831f2036f3f57d256afa17
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-05-04 14:37:26 +0700

    :pencil: :penguin: request permission location

--- app/src/main/java/com/yoesuv/infomadiun/menu/maps/views/FragmentMaps.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/maps/views/FragmentMaps.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.IntentSender
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -24,8 +25,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.yoesuv.infomadiun.R
 import com.yoesuv.infomadiun.data.Constants
 import com.yoesuv.infomadiun.menu.maps.contracts.MapContract
@@ -38,8 +39,6 @@ import kotlinx.android.synthetic.main.activity_main.view.*
  */
 class FragmentMaps: Fragment(), OnMapReadyCallback, MapContract.ViewMaps {
 
-
-
     companion object {
         const val REQUEST_FEATURE_LOCATION_PERMISSION_CODE:Int = 12
         fun getInstance():Fragment{
@@ -50,6 +49,7 @@ class FragmentMaps: Fragment(), OnMapReadyCallback, MapContract.ViewMaps {
     private lateinit var activity: Activity
     private lateinit var mapPresenter: MapPresenter
     private var googleMap: GoogleMap? = null
+    private lateinit var rxPermission: RxPermissions
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_map, container, false)
@@ -60,7 +60,9 @@ class FragmentMaps: Fragment(), OnMapReadyCallback, MapContract.ViewMaps {
         val toolbar = activity.findViewById<Toolbar>(R.id.toolbarMain)
         toolbar.textViewToolbar.text = getString(R.string.menu_maps)
 
+        rxPermission = RxPermissions(activity)
         displayLocationSettingsRequest()
+        requestPermission()
 
         val mapFragment:SupportMapFragment? = childFragmentManager.findFragmentById(R.id.mapLocation) as SupportMapFragment
         mapFragment?.getMapAsync(this)
@@ -117,8 +119,20 @@ class FragmentMaps: Fragment(), OnMapReadyCallback, MapContract.ViewMaps {
         }
     }
 
+    private fun requestPermission(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            rxPermission.request(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    .subscribe { t: Boolean? ->
+                        if(t!!){
+                            Log.d(Constants.TAG_DEBUG,"FragmentMaps # requestPermission GRANTED")
+                        }else{
+                            Log.d(Constants.TAG_ERROR,"FragmentMaps # requestPermission DENIED")
+                        }
+                    }
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap?) {
-        Log.d(Constants.TAG_DEBUG,"FragmentMaps # Map Ready")
         googleMap?.clear()
 
         val settings = googleMap?.uiSettings
@@ -144,7 +158,6 @@ class FragmentMaps: Fragment(), OnMapReadyCallback, MapContract.ViewMaps {
     }
 
     override fun setData(listPin: MutableList<PinModel>) {
-        Log.d(Constants.TAG_DEBUG,"FragmentMaps # jumlah pin ${listPin.size}")
         if(listPin.isNotEmpty()){
             for(i:Int in 0..listPin.size){
                 val marker = MarkerOptions()
