70dbdc5b42290927937c4fd5ca090c48b8b12480
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-05-07 10:28:06 +0700

    :construction: :penguin: set app info icon & name

--- app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentInfo.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentInfo.kt
@@ -2,15 +2,23 @@ package com.yoesuv.infomadiun.menu.other.views
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yoesuv.infomadiun.BuildConfig
 import com.yoesuv.infomadiun.R
+import kotlinx.android.synthetic.main.child_fragment_info.view.*
 
 class ChildFragmentInfo: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = LayoutInflater.from(context).inflate(R.layout.child_fragment_info, container, false)
+
+        view.imageViewAppInfo.setImageDrawable(ContextCompat.getDrawable(context!!, R.drawable.ic_app_info))
+        val version = resources.getString(R.string.info_app_version, BuildConfig.VERSION_NAME)
+        view.textViewVersion.text = version
+
         return view
     }
 
