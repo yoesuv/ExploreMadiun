981d011a508a840c54848b4d3c3f5b1e496b5e34
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-05-07 11:19:06 +0700

    :pencil: :penguin: set layout child fragment changelog

--- app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentChangelog.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentChangelog.kt
@@ -15,6 +15,13 @@ class ChildFragmentChangelog: Fragment() {
         val view: View = LayoutInflater.from(context).inflate(R.layout.child_fragment_changelog, container, false)
 
         view.textViewChangelogOneDesc.text = AppHelper.fromHtml(getString(R.string.ver_1_info))
+        view.textViewChangelogTwoDesc.text = AppHelper.fromHtml(getString(R.string.ver_2_info))
+        view.textViewChangelogThreeDesc.text = AppHelper.fromHtml(getString(R.string.ver_3_info))
+        view.textViewChangelogFourDesc.text = AppHelper.fromHtml(getString(R.string.ver_4_info))
+        view.textViewChangelogFiveDesc.text = AppHelper.fromHtml(getString(R.string.ver_5_info))
+        view.textViewChangelogSixDesc.text = AppHelper.fromHtml(getString(R.string.ver_6_info))
+        view.textViewChangelogSevenDesc.text = AppHelper.fromHtml(getString(R.string.ver_7_info))
+        view.textViewChangelogEightDesc.text = AppHelper.fromHtml(getString(R.string.ver_8_info))
 
         return view
     }
