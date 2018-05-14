561bc62d84762e043750b9b544880e2e2b00b284
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-05-14 09:43:55 +0700

    :racehorse: :penguin: improve fragment other

--- app/src/main/java/com/yoesuv/infomadiun/menu/other/views/FragmentOther.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/views/FragmentOther.kt
@@ -5,17 +5,16 @@ import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gigamole.navigationtabstrip.NavigationTabStrip
 import com.yoesuv.infomadiun.R
 import com.yoesuv.infomadiun.menu.other.adapters.TabOtherAdapter
 import com.yoesuv.infomadiun.utils.ZoomOutPageTransformer
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_other.view.*
 
 /**
  *  Created by yusuf on 4/30/18.
@@ -35,24 +34,24 @@ class FragmentOther: Fragment() {
 
         val activity: AppCompatActivity = activity as AppCompatActivity
         appBarLayout = activity.findViewById(R.id.mainAppBar)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             appBarLayout.elevation = 0f
+            view.navigationTabStrip.elevation = 5f
         }
         val toolbar = activity.findViewById<Toolbar>(R.id.toolbarMain)
         toolbar.textViewToolbar.text = getString(R.string.menu_other)
 
-        val tab:NavigationTabStrip = view.findViewById(R.id.navigationTabStrip)
-        val viewPager: ViewPager = view.findViewById(R.id.viewPagerOther)
+        view.viewPagerOther.adapter = TabOtherAdapter(childFragmentManager)
+        view.viewPagerOther.setPageTransformer(true, ZoomOutPageTransformer())
 
-        viewPager.adapter = TabOtherAdapter(childFragmentManager)
-        viewPager.setPageTransformer(true, ZoomOutPageTransformer())
-        tab.setViewPager(viewPager)
-        tab.setTitles(getString(R.string.informasi), getString(R.string.changelog), getString(R.string.thanks_to), getString(R.string.library))
-        tab.inactiveColor = ContextCompat.getColor(context!!, R.color.grey_50)
-        tab.activeColor = ContextCompat.getColor(context!!, R.color.white)
-        tab.stripColor = ContextCompat.getColor(context!!, R.color.colorAccent)
-        tab.titleSize = 25f
-        tab.cornersRadius = 0f
+        view.navigationTabStrip.setViewPager(view.viewPagerOther)
+        view.navigationTabStrip.setTitles(getString(R.string.informasi), getString(R.string.changelog), getString(R.string.thanks_to), getString(R.string.library))
+        view.navigationTabStrip.inactiveColor = ContextCompat.getColor(context!!, R.color.grey_50)
+        view.navigationTabStrip.activeColor = ContextCompat.getColor(context!!, R.color.white)
+        view.navigationTabStrip.stripColor = ContextCompat.getColor(context!!, R.color.colorAccent)
+        view.navigationTabStrip.titleSize = 25f
+        view.navigationTabStrip.cornersRadius = 0f
 
         return view
     }
