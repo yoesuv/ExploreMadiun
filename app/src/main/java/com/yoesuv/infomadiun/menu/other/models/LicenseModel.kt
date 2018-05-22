e3f2898497859e61558986cbe4a5caed696255b9
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-05-22 14:59:41 +0700

    :pencil: :penguin: change fragment child library to recyclerView

---
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/models/LicenseModel.kt
@@ -0,0 +1,8 @@
+package com.yoesuv.infomadiun.menu.other.models
+
+class LicenseModel(
+        val title: String?,
+        val url: String?,
+        val license: String?,
+        val isLast: Boolean?
+)
--- app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentLibraries.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentLibraries.kt
@@ -3,11 +3,13 @@ package com.yoesuv.infomadiun.menu.other.views
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yoesuv.infomadiun.R
-import com.yoesuv.infomadiun.utils.AppHelper
+import com.yoesuv.infomadiun.menu.other.adapters.LicenseAdapter
+import com.yoesuv.infomadiun.menu.other.models.LicenseModel
 import kotlinx.android.synthetic.main.child_fragment_libraries.view.*
 
 class ChildFragmentLibraries: Fragment() {
@@ -18,23 +20,42 @@ class ChildFragmentLibraries: Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.child_fragment_libraries, container, false)
+    private var listLibraries: MutableList<LicenseModel> = arrayListOf()
 
-        view.textViewIcons8License.text = AppHelper.fromHtml(getString(R.string.icons8_license))
-        view.textViewRetrofitLicense.text = AppHelper.fromHtml(getString(R.string.retrofit_license))
-        view.textViewRxJavaLicense.text = AppHelper.fromHtml(getString(R.string.rxjava_license))
-        view.textViewRxAndroidLicense.text = AppHelper.fromHtml(getString(R.string.rxAndroid_license))
-        view.textViewRxKotlinLicense.text = AppHelper.fromHtml(getString(R.string.rx_kotlin_license))
-        view.textViewGlideLicense.text = AppHelper.fromHtml(getString(R.string.glide_license))
-        view.textViewPhotoViewLicense.text = AppHelper.fromHtml(getString(R.string.photoview_license))
-        view.textViewRxPermissionLicense.text = AppHelper.fromHtml(getString(R.string.rx_permission_license))
-        view.textViewToastyLicense.text = AppHelper.fromHtml(getString(R.string.toasty_license))
-        view.textViewNavigationTabStripLicense.text = AppHelper.fromHtml(getString(R.string.navigation_tab_strip_license))
-        view.textViewGoogleDirectionLicense.text = AppHelper.fromHtml(getString(R.string.google_direction_license))
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View? = LayoutInflater.from(context).inflate(R.layout.child_fragment_libraries, container, false)
+
+        view?.recyclerViewLicense?.layoutManager = LinearLayoutManager(context)
+
+        val icons8 = LicenseModel(context?.getString(R.string.icons8), context?.getString(R.string.icons8_url), context?.getString(R.string.icons8_license), false)
+        val retrofit = LicenseModel(context?.getString(R.string.retrofit), context?.getString(R.string.retrofit_url), context?.getString(R.string.retrofit_license), false)
+        val rxJava = LicenseModel(context?.getString(R.string.rxjava), context?.getString(R.string.rxjava_url), context?.getString(R.string.rxjava_license), false)
+        val rxAndroid = LicenseModel(context?.getString(R.string.rxAndroid), context?.getString(R.string.rxAndroid_url), context?.getString(R.string.rxAndroid_license), false)
+        val rxKotlin = LicenseModel(context?.getString(R.string.rx_kotlin), context?.getString(R.string.rx_kotlin_url), context?.getString(R.string.rx_kotlin_license), false)
+        val glide = LicenseModel(context?.getString(R.string.glide), context?.getString(R.string.glide_url), context?.getString(R.string.glide_license), false)
+        val photoView = LicenseModel(context?.getString(R.string.photoview), context?.getString(R.string.photoview_url), context?.getString(R.string.photoview_license), false)
+        val rxPermission = LicenseModel(context?.getString(R.string.rx_permission), context?.getString(R.string.rx_permission_url), context?.getString(R.string.rx_permission_license), false)
+        val toasty = LicenseModel(context?.getString(R.string.toasty), context?.getString(R.string.toasty_url), context?.getString(R.string.toasty_license), false)
+        val navigationTabStrip = LicenseModel(context?.getString(R.string.navigation_tab_strip), context?.getString(R.string.navigation_tab_strip_url), context?.getString(R.string.navigation_tab_strip_license), false)
+        val googleDirection = LicenseModel(context?.getString(R.string.google_direction), context?.getString(R.string.google_direction_url), context?.getString(R.string.google_direction_license), true)
+
+        listLibraries.add(icons8)
+        listLibraries.add(retrofit)
+        listLibraries.add(rxJava)
+        listLibraries.add(rxAndroid)
+        listLibraries.add(rxKotlin)
+        listLibraries.add(glide)
+        listLibraries.add(photoView)
+        listLibraries.add(rxPermission)
+        listLibraries.add(toasty)
+        listLibraries.add(navigationTabStrip)
+        listLibraries.add(googleDirection)
+
+        val adapter = LicenseAdapter(context, listLibraries)
+        view?.recyclerViewLicense?.adapter = adapter
 
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
-            view.nestedScrollViewLibraries.isNestedScrollingEnabled = true
+            view?.recyclerViewLicense?.isNestedScrollingEnabled = true
         }
 
         return view
