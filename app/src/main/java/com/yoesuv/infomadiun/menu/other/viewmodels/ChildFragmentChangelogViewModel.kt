e6fc8ea1dbd306e58404b4823f490296ea40bc29
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-08-31 10:24:56 +0700

    :pencil: :penguin: convert child fragment changelog to data binding

---
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/viewmodels/ChildFragmentChangelogViewModel.kt
@@ -0,0 +1,29 @@
+package com.yoesuv.infomadiun.menu.other.viewmodels
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.content.Context
+import com.yoesuv.infomadiun.R
+import com.yoesuv.infomadiun.menu.other.models.ChangeLogModel
+
+class ChildFragmentChangelogViewModel: ViewModel() {
+
+    private var listChangelog: MutableList<ChangeLogModel> = mutableListOf()
+    var listData: MutableLiveData<MutableList<ChangeLogModel>> = MutableLiveData()
+
+    fun setupData(context: Context?){
+        listChangelog.clear()
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_9), context?.getString(R.string.ver_9_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_8), context?.getString(R.string.ver_8_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_7), context?.getString(R.string.ver_7_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_6), context?.getString(R.string.ver_6_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_5), context?.getString(R.string.ver_5_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_4), context?.getString(R.string.ver_4_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_3), context?.getString(R.string.ver_3_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_2), context?.getString(R.string.ver_2_info), false))
+        listChangelog.add(ChangeLogModel(context?.getString(R.string.ver_1), context?.getString(R.string.ver_1_info), true))
+
+        listData.value = listChangelog
+    }
+
+}
--- app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentChangelog.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentChangelog.kt
@@ -1,5 +1,7 @@
 package com.yoesuv.infomadiun.menu.other.views
 
+import android.arch.lifecycle.Observer
+import android.databinding.DataBindingUtil
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -8,8 +10,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yoesuv.infomadiun.R
+import com.yoesuv.infomadiun.databinding.ChildFragmentChangelogBinding
 import com.yoesuv.infomadiun.menu.other.adapters.ChangeLogAdapter
 import com.yoesuv.infomadiun.menu.other.models.ChangeLogModel
+import com.yoesuv.infomadiun.menu.other.viewmodels.ChildFragmentChangelogViewModel
 import com.yoesuv.infomadiun.utils.AppHelper
 import kotlinx.android.synthetic.main.child_fragment_changelog.view.*
 
@@ -21,33 +25,37 @@ class ChildFragmentChangelog: Fragment() {
         }
     }
 
+    private lateinit var binding: ChildFragmentChangelogBinding
+    private lateinit var viewModel: ChildFragmentChangelogViewModel
+    private lateinit var adapter: ChangeLogAdapter
     private var listChangelog: MutableList<ChangeLogModel> = arrayListOf()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_9), getString(R.string.ver_9_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_8), getString(R.string.ver_8_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_7), getString(R.string.ver_7_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_6), getString(R.string.ver_6_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_5), getString(R.string.ver_5_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_4), getString(R.string.ver_4_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_3), getString(R.string.ver_3_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_2), getString(R.string.ver_2_info), false))
-        listChangelog.add(ChangeLogModel(getString(R.string.ver_1), getString(R.string.ver_1_info), true))
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.child_fragment_changelog, container, false)
+        viewModel = ChildFragmentChangelogViewModel()
+        binding.changelog = viewModel
+
+        setupRecycler()
+        viewModel.setupData(context)
+        viewModel.listData.observe(this, Observer {
+            onListDataChanged(it!!)
+        })
+
+        return binding.root
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.child_fragment_changelog, container, false)
+    private fun setupRecycler(){
+        binding.recyclerViewChangelog.layoutManager = LinearLayoutManager(context)
+        adapter = ChangeLogAdapter(context, listChangelog)
+        binding.recyclerViewChangelog.adapter = adapter
+    }
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
-            view.recyclerViewChangelog.isNestedScrollingEnabled = true
+    private fun onListDataChanged(listData: MutableList<ChangeLogModel>){
+        listChangelog.clear()
+        listChangelog.addAll(listData)
+        binding.recyclerViewChangelog.post {
+            adapter.notifyDataSetChanged()
         }
-
-        view.recyclerViewChangelog.layoutManager = LinearLayoutManager(context)
-        val adapter = ChangeLogAdapter(context, listChangelog)
-        view.recyclerViewChangelog.adapter = adapter
-
-        return view
     }
 
 }
