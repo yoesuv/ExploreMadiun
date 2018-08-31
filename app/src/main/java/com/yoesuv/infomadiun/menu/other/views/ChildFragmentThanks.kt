3445275ea7e3bc72847ca59c7a5ce6f57c419428
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-08-31 10:08:28 +0700

    :pencil: :penguin: convert child fragment thanks to data binding

--- app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentThanks.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentThanks.kt
@@ -1,13 +1,14 @@
 package com.yoesuv.infomadiun.menu.other.views
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yoesuv.infomadiun.R
+import com.yoesuv.infomadiun.databinding.ChildFragmentThanksBinding
 import com.yoesuv.infomadiun.utils.AppHelper
-import kotlinx.android.synthetic.main.child_fragment_thanks.view.*
 
 class ChildFragmentThanks: Fragment(){
 
@@ -17,10 +18,12 @@ class ChildFragmentThanks: Fragment(){
         }
     }
 
+    private lateinit var binding: ChildFragmentThanksBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.child_fragment_thanks, container, false)
-        view.textViewThanks.text = AppHelper.fromHtml(getString(R.string.trims))
-        return view
+        binding = DataBindingUtil.inflate(inflater, R.layout.child_fragment_thanks, container, false)
+        binding.textViewThanks.text = AppHelper.fromHtml(getString(R.string.trims))
+        return binding.root
     }
 
 }
