b7ce73deedb4f73c14484bfb3f895cd6b10d3c29
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-08-31 10:58:14 +0700

    :pencil: :penguin: convert item list place to data binding

--- app/src/main/java/com/yoesuv/infomadiun/menu/listplace/adapters/ListPlaceAdapter.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/listplace/adapters/ListPlaceAdapter.kt
@@ -1,32 +1,15 @@
 package com.yoesuv.infomadiun.menu.listplace.adapters
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.app.Activity
-import android.app.AlertDialog
-import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.os.Build
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.*
-import android.widget.ImageView
-import com.bumptech.glide.GenericTransitionOptions
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DecodeFormat
-import com.bumptech.glide.request.RequestOptions
 import com.yoesuv.infomadiun.R
-import com.yoesuv.infomadiun.data.Constants
+import com.yoesuv.infomadiun.databinding.ItemListPlaceBinding
 import com.yoesuv.infomadiun.main.TransparentActivity
 import com.yoesuv.infomadiun.menu.listplace.models.PlaceModel
-import kotlinx.android.synthetic.main.item_list_place.view.*
-import kotlinx.android.synthetic.main.popup_detail_list_place.view.*
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
+import com.yoesuv.infomadiun.menu.listplace.viewmodels.ItemListPlaceViewModel
 
 /**
  *  Created by yusuf on 4/30/18.
@@ -36,8 +19,8 @@ class ListPlaceAdapter(private val activity: Activity, private val listPlace:Mut
     private var inflater: LayoutInflater = LayoutInflater.from(activity)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view:View = inflater.inflate(R.layout.item_list_place, parent, false)
-        return ViewHolder(view)
+        val binding: ItemListPlaceBinding = DataBindingUtil.inflate(inflater, R.layout.item_list_place, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -47,49 +30,10 @@ class ListPlaceAdapter(private val activity: Activity, private val listPlace:Mut
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val fixPos:Int = holder.adapterPosition
         holder.setData(listPlace[fixPos])
-        holder.itemView.setOnClickListener {
-            showPopUp(activity, listPlace[fixPos])
-        }
         holder.itemView.setOnLongClickListener {
             showPopUpImage(activity, listPlace[fixPos])
             return@setOnLongClickListener true
         }
-    }
-
-    private fun showPopUp(activity: Activity?, model: PlaceModel){
-        val alertDialog:AlertDialog
-        val ab:AlertDialog.Builder = AlertDialog.Builder(activity)
-        val view:View = LayoutInflater.from(activity?.applicationContext).inflate(R.layout.popup_detail_list_place, null)
-        ab.setView(view)
-        view.textViewPopUpListPlaceName.text = model.name
-        view.textViewPopUpListPlaceDescription.text = model.description
-        view.imageViewPopupListPlace.scaleType = ImageView.ScaleType.CENTER_CROP
-        Glide.with(activity?.applicationContext)
-                .load(model.image)
-                .apply(RequestOptions()
-                        .placeholder(R.drawable.placeholder_image)
-                        .error(R.drawable.placeholder_image)
-                        .format(DecodeFormat.PREFER_ARGB_8888))
-                .transition(GenericTransitionOptions.with(android.R.anim.fade_in))
-                .into(view.imageViewPopupListPlace)
-        alertDialog = ab.create()
-        alertDialog.setOnShowListener {
-            Log.d(Constants.TAG_DEBUG,"ListPlaceAdapter # dialog on show")
-            revealShow(view, true, alertDialog)
-        }
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
-            alertDialog.setOnKeyListener(DialogInterface.OnKeyListener { _, i, _ ->
-                if (i == KeyEvent.KEYCODE_BACK) {
-                    revealShow(view, false, alertDialog)
-                    return@OnKeyListener true
-                }
-                false
-            })
-            alertDialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            alertDialog.setCancelable(false)
-            alertDialog.setCanceledOnTouchOutside(false)
-        }
-        alertDialog.show()
     }
 
     private fun showPopUpImage(activity: Activity?, model: PlaceModel){
@@ -99,46 +43,10 @@ class ListPlaceAdapter(private val activity: Activity, private val listPlace:Mut
         activity?.startActivity(intent)
     }
 
-    private fun revealShow(view: View, reveal: Boolean, alertDialog:AlertDialog){
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
-            val w: Double = view.width.toDouble() - (view.width.toDouble()/2)
-            val h: Double = view.height.toDouble() - (view.height.toDouble()/2)
-            val endRadius: Float = Math.hypot(w, h).toFloat()
-
-            if(reveal) {
-                val anim: Animator = ViewAnimationUtils.createCircularReveal(view, w.toInt(), h.toInt(), 0F, endRadius)
-                anim.start()
-            }else{
-                val anim: Animator = ViewAnimationUtils.createCircularReveal(view, w.toInt(), h.toInt(), endRadius, 0F)
-                anim.addListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        super.onAnimationEnd(animation)
-                        alertDialog.dismiss()
-                    }
-                })
-                anim.start()
-            }
-        }
-    }
-
-    class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(val binding: ItemListPlaceBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun setData(placeModel: PlaceModel){
-            launch {
-                withContext(UI){
-                    itemView.textViewItemListPlaceName.text = placeModel.name
-                    itemView.textViewItemListPlaceLocation.text = placeModel.location
-                    itemView.imageViewItemListPlace.scaleType = ImageView.ScaleType.CENTER_CROP
-                    Glide.with(itemView)
-                            .load(placeModel.image)
-                            .apply(RequestOptions()
-                                    .placeholder(R.drawable.placeholder_image)
-                                    .error(R.drawable.placeholder_image)
-                                    .format(DecodeFormat.PREFER_ARGB_8888))
-                            .transition(GenericTransitionOptions.with(android.R.anim.fade_in))
-                            .into(itemView.imageViewItemListPlace)
-                }
-            }
+            binding.itemListPlace = ItemListPlaceViewModel(placeModel)
         }
 
     }
---
+++ app/src/main/java/com/yoesuv/infomadiun/menu/listplace/viewmodels/ItemListPlaceViewModel.kt
@@ -0,0 +1,12 @@
+package com.yoesuv.infomadiun.menu.listplace.viewmodels
+
+import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import com.yoesuv.infomadiun.menu.listplace.models.PlaceModel
+
+class ItemListPlaceViewModel(placeModel: PlaceModel): ViewModel() {
+
+    var name: ObservableField<String> = ObservableField(placeModel.name!!)
+    var location: ObservableField<String> = ObservableField(placeModel.location!!)
+
+}
