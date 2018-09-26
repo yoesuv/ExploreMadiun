15ead60f444e3ff5e51fdb0bae4bd364d748b777
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-09-26 10:14:50 +0700

    :pencil: :penguin: set click link github

--- app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentInfo.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/views/ChildFragmentInfo.kt
@@ -1,6 +1,8 @@
 package com.yoesuv.infomadiun.menu.other.views
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -25,6 +27,10 @@ class ChildFragmentInfo: Fragment() {
 
         val version = resources.getString(R.string.info_app_version, BuildConfig.VERSION_NAME)
         binding.textViewVersion.text = version
+        binding.textViewGithub.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.github_url)))
+            context?.startActivity(intent)
+        }
 
         return binding.root
     }
