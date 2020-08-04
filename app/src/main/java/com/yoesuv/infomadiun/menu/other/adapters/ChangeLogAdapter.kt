6500fbc830f804d58dab509d9f2a9e6e240d8037
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-08-04 12:01:47 +0700

    :racehorse: improve list changelog

--- app/src/main/java/com/yoesuv/infomadiun/menu/other/adapters/ChangeLogAdapter.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/adapters/ChangeLogAdapter.kt
@@ -1,36 +1,24 @@
 package com.yoesuv.infomadiun.menu.other.adapters
 
 import android.content.Context
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.yoesuv.infomadiun.R
-import com.yoesuv.infomadiun.databinding.ItemChangelogBinding
+import com.yoesuv.infomadiun.menu.other.adapters.viewholders.ChangeLogViewHolder
 import com.yoesuv.infomadiun.menu.other.models.ChangeLogModel
-import com.yoesuv.infomadiun.menu.other.viewmodels.ItemChangeLogViewModel
 
-class ChangeLogAdapter(val context: Context?, var listChangelog: MutableList<ChangeLogModel>): RecyclerView.Adapter<ChangeLogAdapter.ChangelogViewHolder>(){
+class ChangeLogAdapter(val context: Context?, var listChangelog: MutableList<ChangeLogModel>): RecyclerView.Adapter<ChangeLogViewHolder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChangelogViewHolder {
-        val binding: ItemChangelogBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.item_changelog, parent, false)
-        return ChangelogViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChangeLogViewHolder {
+        return ChangeLogViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int {
         return listChangelog.size
     }
 
-    override fun onBindViewHolder(holder: ChangelogViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ChangeLogViewHolder, position: Int) {
         val fixPosition = holder.adapterPosition
-        holder.setupData(listChangelog[fixPosition])
+        holder.bind(listChangelog[fixPosition])
     }
 
-    class ChangelogViewHolder(val binding: ItemChangelogBinding) : RecyclerView.ViewHolder(binding.root) {
-
-        fun setupData(model: ChangeLogModel){
-            binding.itemChangelog = ItemChangeLogViewModel(model)
-        }
-
-    }
 }
---
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/adapters/viewholders/ChangeLogViewHolder.kt
@@ -0,0 +1,25 @@
+package com.yoesuv.infomadiun.menu.other.adapters.viewholders
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yoesuv.infomadiun.databinding.ItemChangelogBinding
+import com.yoesuv.infomadiun.menu.other.models.ChangeLogModel
+import com.yoesuv.infomadiun.menu.other.viewmodels.ItemChangeLogViewModel
+
+class ChangeLogViewHolder(val binding: ItemChangelogBinding): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(changeLogModel: ChangeLogModel) {
+        binding.itemChangelog = ItemChangeLogViewModel(changeLogModel)
+        binding.executePendingBindings()
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): ChangeLogViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemChangelogBinding.inflate(inflater, parent, false)
+            return ChangeLogViewHolder(binding)
+        }
+    }
+
+}
