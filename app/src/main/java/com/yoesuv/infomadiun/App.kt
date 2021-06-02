385a5d2850012482ad258f30a4afd0ba69a29b17
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-06-02 20:11:43 +0700

    :arrow_up: update sentry

--- app/src/main/java/com/yoesuv/infomadiun/App.kt
+++ app/src/main/java/com/yoesuv/infomadiun/App.kt
@@ -2,9 +2,11 @@ package com.yoesuv.infomadiun
 
 import android.app.Application
 import com.yoesuv.infomadiun.utils.PreferencesHelper
+import io.sentry.SentryLevel
+import io.sentry.android.core.SentryAndroid
 
 /**
- *  Created by yusuf on 4/28/18.
+ *  Updated by yusuf on 02 June 2021.
  */
 class App : Application() {
 
@@ -15,6 +17,21 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         prefHelper = PreferencesHelper(this)
+        initSentry()
+    }
+
+    private fun initSentry() {
+        if (!BuildConfig.DEBUG) {
+            SentryAndroid.init(this) { options ->
+                options.setBeforeSend { event, _ ->
+                    if (SentryLevel.DEBUG == event.level) {
+                        return@setBeforeSend null
+                    } else {
+                        return@setBeforeSend event
+                    }
+                }
+            }
+        }
     }
 
 }
