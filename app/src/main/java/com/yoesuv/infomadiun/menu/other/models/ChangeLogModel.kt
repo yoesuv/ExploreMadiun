1eeb386c0e6338d89defc796a6e7dccef0431ad5
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-03-02 16:37:39 +0700

    :pencil: add adapter callback

--- app/src/main/java/com/yoesuv/infomadiun/menu/other/models/ChangeLogModel.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/other/models/ChangeLogModel.kt
@@ -1,5 +1,8 @@
 package com.yoesuv.infomadiun.menu.other.models
 
+import com.google.errorprone.annotations.Keep
+
+@Keep
 data class ChangeLogModel(
         val title: String?,
         val description: String?,
