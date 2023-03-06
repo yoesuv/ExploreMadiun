123695e573a9da0c4a74bbd1bcb22d07c668fad4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-03-06 14:46:39 +0700

    :pencil: update decrypt

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="API_KEY_PASSPHRASE" \
 --output $HOME/secrets/apiKey.properties apiKey.properties.gpg
