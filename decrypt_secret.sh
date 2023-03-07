7803c47e5c85f20df04db45e9cab6ea1e86f0a38
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-03-07 10:09:16 +0700

    :fire: delete old encrypt file

--- decrypt_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="API_KEY_PASSPHRASE" \
---output $HOME/secrets/apiKey.properties apiKey.properties.gpg
