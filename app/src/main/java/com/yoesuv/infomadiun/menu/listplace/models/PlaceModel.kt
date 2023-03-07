ea36f5d4231c431fe40007faf82c756590bcfbfc
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-03-07 15:06:10 +0700

    :hammer:  remove unused & update parcelize

--- app/src/main/java/com/yoesuv/infomadiun/menu/listplace/models/PlaceModel.kt
+++ app/src/main/java/com/yoesuv/infomadiun/menu/listplace/models/PlaceModel.kt
@@ -6,7 +6,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 /**
  *  Created by yusuf on 4/30/18.
