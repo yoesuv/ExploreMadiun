c2d09cceae2267928c236b055835ce6fd4b5a4f6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-21 15:42:14 +0700

    :hammer: refactor request api

--- app/src/main/java/com/yoesuv/infomadiun/networks/AppRepository.kt
+++ app/src/main/java/com/yoesuv/infomadiun/networks/AppRepository.kt
@@ -3,26 +3,20 @@ package com.yoesuv.infomadiun.networks
 import com.yoesuv.infomadiun.menu.gallery.models.GalleryModel
 import com.yoesuv.infomadiun.menu.listplace.models.PlaceModel
 import com.yoesuv.infomadiun.menu.maps.models.PinModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import java.lang.Exception
 
-class AppRepository(private val scope: CoroutineScope): SafeApiRequest() {
+class AppRepository {
 
     private val restApi = ServiceFactory.create()
 
-    fun getSplashData(onSuccess:(MutableList<PlaceModel>?, MutableList<GalleryModel>?, MutableList<PinModel>?) -> Unit, onError:(Exception) -> Unit) {
-        scope.launch {
-            try {
-                val resultListPlace = async { apiRequest { restApi.listPlace() } }
-                val resultGallery = async { apiRequest { restApi.gallery() } }
-                val resultMapPins = async { apiRequest { restApi.mapPins() } }
-
-                onSuccess(resultListPlace.await(), resultGallery.await(), resultMapPins.await())
-            } catch (e: Exception) {
-                onError(e)
-            }
+    suspend fun getSplashData(onSuccess: (MutableList<PlaceModel>?, MutableList<GalleryModel>?, MutableList<PinModel>?) -> Unit, onError: (Exception) -> Unit) {
+        try {
+            val resultListPlace = restApi.listPlace()
+            val resultGallery = restApi.gallery()
+            val resultMapPins = restApi.mapPins()
+            onSuccess(resultListPlace.body(), resultGallery.body(), resultMapPins.body())
+        } catch (e: Exception) {
+            onError(e)
         }
     }
 
