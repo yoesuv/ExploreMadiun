44a9f96e2e1942dd0053bba9d238dd10df53c248
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-07-10 15:30:14 +0700

    Adds window insets padding utility
    Adds a utility function to apply window insets padding to a view.
    
    The `insetsPadding` function allows applying padding to a view based on system window insets, such as the status bar and navigation bar. It also allows setting a background color for the view. This helps to avoid UI elements being obscured by system bars.
    Also adds function to check API level for VanillaIceCream.

--- app/src/main/java/com/yoesuv/infomadiun/utils/AppHelper.kt
+++ app/src/main/java/com/yoesuv/infomadiun/utils/AppHelper.kt
@@ -6,8 +6,12 @@ import android.content.IntentSender
 import android.location.LocationManager
 import android.os.Build
 import android.text.Html
+import android.view.View
 import android.widget.Toast
+import androidx.annotation.ColorInt
 import androidx.annotation.StringRes
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.LocationRequest
@@ -74,6 +78,32 @@ object AppHelper {
             }
         } else {
             ""
+        }
+    }
+
+    fun isVanillaIceCreamAndUp(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM
+    }
+
+    fun insetsPadding(
+        view: View, left: Boolean = false,
+        top: Boolean = false,
+        right: Boolean = false,
+        bottom: Boolean = false,
+        @ColorInt color: Int? = null,
+    ) {
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowInset ->
+            val inset = windowInset.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                if (left) inset.left else v.paddingLeft,
+                if (top) inset.top else v.paddingTop,
+                if (right) inset.right else v.paddingRight,
+                if (bottom) inset.bottom + 32 else v.paddingBottom
+            )
+            color?.let {
+                v.setBackgroundColor(it)
+            }
+            windowInset
         }
     }
 
