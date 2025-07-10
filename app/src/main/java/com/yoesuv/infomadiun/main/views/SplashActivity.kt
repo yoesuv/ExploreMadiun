347c98d530eb11832240d2084bc6a0dd59264d63
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-07-10 16:22:39 +0700

    Improves immersive mode and UI enhancements.
    Enables edge-to-edge display for a more immersive experience on newer Android versions.
    
    Sets the navigation and status bar to transparent and adjusts padding for the app bar, ensuring content isn't obscured by system UI elements. This provides a cleaner, more modern look and feel.

--- app/src/main/java/com/yoesuv/infomadiun/main/views/SplashActivity.kt
+++ app/src/main/java/com/yoesuv/infomadiun/main/views/SplashActivity.kt
@@ -2,12 +2,14 @@ package com.yoesuv.infomadiun.main.views
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.yoesuv.infomadiun.R
 import com.yoesuv.infomadiun.databinding.ActivitySplashBinding
 import com.yoesuv.infomadiun.main.viewmodels.SplashViewModel
+import com.yoesuv.infomadiun.utils.AppHelper
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -17,6 +19,10 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (AppHelper.isVanillaIceCreamAndUp()) {
+            enableEdgeToEdge()
+        }
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         viewModel = ViewModelProvider(this)[SplashViewModel::class.java]
         binding.splash = viewModel
